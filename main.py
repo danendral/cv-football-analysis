de889ff904f261cf356663ed9a9672e2fc892ebe
danendral <dlohanata@gmail.com>
2026-01-28 22:22:18 +0700

    Adjust position from camera movement

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ def main():
                                        read_from_stub=True,
                                        stub_path='stubs/tracks_stubs.pkl')
     
+    # Get object positions
+    tracker.add_position_to_tracks(tracks)
+    
     # Camera Movement Estimator
     camera_movement_estimator = CameraMovementEstimator(video_frames[0])
     camera_movement_per_frame = camera_movement_estimator.get_camera_movement(
@@ -24,6 +27,8 @@ def main():
         read_from_stub=True,
         stub_path='stubs/camera_movement_stub.pkl'
     )
+    camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
+
     
     # Interpolate Ball Positions
     tracks['ball'] = tracker.interpolate_ball_positions(tracks['ball'])
--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -33,3 +33,8 @@ def measure_xy_distance(p1, p2):
     dx = p1[0] - p2[0]
     dy = p1[1] - p2[1]
     return dx, dy
+
+
+def get_foot_position(bbox):
+    x1, y1, x2, y2 = bbox
+    return int((x1+x2)/2), int(y2)
