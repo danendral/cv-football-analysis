255791aa9c1cb7f3a4ad4e9b5a16629f638e6f7f
danendral <dlohanata@gmail.com>
2026-02-08 14:11:55 +0700

    Commit

--- camera_movement_estimator/camera_movement_estimator.py
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -37,7 +37,8 @@ class CameraMovementEstimator():
                     camera_movement = camera_movement_per_frame[frame_num]
                     position_adjusted = (position[0]-camera_movement[0], position[1]-camera_movement[1])
                     tracks[object][frame_num][track_id]['position_adjusted'] = position_adjusted
-        
+
+
 
     def get_camera_movement(self, frames, read_from_stub=False, stub_path=None):
         # Read the stub if it exists
