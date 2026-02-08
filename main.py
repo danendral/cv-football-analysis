06b557c50d1e7ce5a770e654f0e332010576a1b8
danendral <dlohanata@gmail.com>
2026-02-08 14:38:28 +0700

    Add transform point in view transformer

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 import numpy as np
 from camera_movement_estimator import CameraMovementEstimator
+from view_transformer import ViewTransformer
 
 def main():
     # Read Video
@@ -28,6 +29,10 @@ def main():
         stub_path='stubs/camera_movement_stub.pkl'
     )
     camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
+
+    # View Transformer
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
 
     
     # Interpolate Ball Positions
